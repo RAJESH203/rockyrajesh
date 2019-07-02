194c9046b48760bc8a48ae84df56a5b900807335
RAJESH203 <37853368+RAJESH203@users.noreply.github.com>
2019-07-02 15:28:52 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,8 +1,8 @@
-a=input("enter no")
+a=input("")
 a=float(a)
 if a>0:
-  print("positive")
+  print("Positive")
 elif a<0:
-  print("negative")
+  print("Negative")
 else:
-   print("zero") 
+   print("Zero") 
