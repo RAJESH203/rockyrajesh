ef1183a5dbd4b66b171051dfa1d0ed76db16850f
RAJESH203 <37853368+RAJESH203@users.noreply.github.com>
2019-07-03 09:57:00 +0530

    Update p3.py

--- p3.py
+++ p3.py
@@ -1,7 +1,8 @@
 str=input("")
 if str in ('a','e','i','o','u'):
   print("Vowel")
-if str in('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'):
-  print("Consonant")
+elif str in('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'):
+   print("Consonant")
 else:
-  print("invalid") 
+    print("invalid")    
+ 
