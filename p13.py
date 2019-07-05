3b877ffa54b08bc23e629cc12c8f2986ec471ab0
RAJESH203 <37853368+RAJESH203@users.noreply.github.com>
2019-07-05 10:18:04 +0530

    Update p13.py

--- p13.py
+++ p13.py
@@ -1,10 +1,10 @@
 a=int(input())
 if a>1:
-  for i in range(2,a):
-    if(a%i==0):
-      print ("no")
-      break
-    else:
-      print("yes")
+ for i in range(2,a):
+  if a%i==0:
+   print ("no")
+   break
+ else:
+   print("yes")
 else:
   print("yes")
